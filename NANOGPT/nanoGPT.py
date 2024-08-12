73a4929edc68afb70e2ac86518c2c1a910591bfa
HanXM0909 <marvel090909@hotmail.com>
2024-08-13 05:29:57 +0800

    0813

--- NANOGPT/nanoGPT.py
+++ NANOGPT/nanoGPT.py
@@ -10,6 +10,7 @@ eval_interval = 300
 learning_rate = 1e-2
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
+n_embd = 32
 
 torch.manual_seed(1337)
 
@@ -52,11 +53,17 @@ def estimate_loss():
     model.train()
     return out
 class BigramLanguageModel(nn.Module):
-    def __init__(self, vocab_size):
+    def __init__(self):
         super().__init__()
-        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.lm_head = nn.Linear(n_embd, vocab_size)
+
     def forward(self, idx, targets=None):
-        logits = self.token_embedding_table(idx)
+        tok_emb = self.token_embedding_table(idx)#(B,T,n_embd)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device))#(T,C)
+        x = tok_emb + pos_emb
+        logits = self.lm_head(x)#(B,T,vocab_size)
         if targets is None:
             loss = None
         else:
@@ -73,7 +80,7 @@ class BigramLanguageModel(nn.Module):
             idx_next = torch.multinomial(probs, num_samples=1)
             idx = torch.cat((idx, idx_next), dim=1)
         return idx
-model = BigramLanguageModel(vocab_size)
+model = BigramLanguageModel()
 m = model.to(device)
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
